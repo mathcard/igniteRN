b7a5ff3c76180a44df778de1962367734308b6e6
mathcard <mathcardoso.94@gmail.com>
2021-06-20 13:47:28 -0300

    card de listagem

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
 import {
   Container,
@@ -12,11 +13,47 @@ import {
   UserGreeting,
   UserName,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 
-
 export function Dashboard() {
+  const data = [
+    {
+      type: 'positive',
+
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020"
+    },
+    {
+      type: 'negative',
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date: "10/04/2020"
+    },
+    {
+      type: 'negative',
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag'
+      },
+      date: "10/04/2020"
+    }
+  ]
+
   return (
     <Container>
       <Header>
@@ -48,7 +85,7 @@ export function Dashboard() {
           amount="R$ 1.259,00"
           lastTransaction="Última saída dia 13 de abril"
         />
-        
+
         <HighlightCard
           type="total"
           title="Total"
@@ -57,6 +94,18 @@ export function Dashboard() {
         />
       </HighlightCards>
 
+      <Transactions>
+        <Title>Listagem</Title>
+        {/*<TransactionCard /> */}
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace()
+          }}
+        />
+      </Transactions>
     </Container>
   )
 }
